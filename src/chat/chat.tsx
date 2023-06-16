73a2d0d219ea47c6b73eabceeffedd08c1556f61
matton <masato_ikegawa612400@yahoo.co.jp>
2023-06-17 00:32:41 +0900

    initialize chat component

---
+++ src/chat/chat.tsx
@@ -0,0 +1,23 @@
+import { useChat } from "ai/react";
+export default function Chat() {
+  const { messages, input, handleInputChange, handleSubmit } = useChat();
+  return (
+    <div className="mx-auto w-full max-w-md py-24 flex flex-col stretch">
+      {messages.map((m) => (
+        <div key={m.id}>
+          {m.role === "user" ? "User: " : "AI: "}
+          {m.content}
+        </div>
+      ))}
+
+      <form onSubmit={handleSubmit}>
+        <input
+          className="fixed w-full max-w-md bottom-0 border border-gray-300 rounded mb-8 shadow-xl p-2"
+          value={input}
+          placeholder="Say something..."
+          onChange={handleInputChange}
+        />
+      </form>
+    </div>
+  );
+}
